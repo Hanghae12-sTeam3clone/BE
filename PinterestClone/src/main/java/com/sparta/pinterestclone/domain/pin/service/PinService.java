b3f3b84d4bc6e5ba3bdac30f3180193b2ae04317
Kuno1734 <dlsgus59@gmail.com>
2023-03-04 13:15:00 +0900

    feat :  PinResponseDto of 메소드 image변수 추가

--- PinterestClone/src/main/java/com/sparta/pinterestclone/domain/pin/service/PinService.java
+++ PinterestClone/src/main/java/com/sparta/pinterestclone/domain/pin/service/PinService.java
@@ -32,24 +32,24 @@ public class PinService {
 
 
     @Transactional
-    public MessageResponseDto create(User user, PinRequestDto pinRequestDto)throws IOException { //  , UserDetailsImpl userDetails 추가
-            String image = "";
-        System.out.println(pinRequestDto.getImage());
-        if(pinRequestDto.getImage() == null){
-            image = "";
-        }else{
+    public MessageResponseDto create(User user, PinRequestDto pinRequestDto) throws IOException { //  , UserDetailsImpl userDetails 추가
+        String image = "";
+//        System.out.println(pinRequestDto.getImage());
+//        if (pinRequestDto.getImage() == null) {
+//            image = "";
+//        } else {
             image = s3Uploader.upload(pinRequestDto.getImage());
-        }
+//        }
         //                      작성 글 저장
         Pin pin = pinRepository.save(Pin.of(pinRequestDto, image, user));        //      , user 추가
 
 //                      PinResponseDto 로 변환 후 responseEntity body 에 담아 반환
         return apiResponse.success(ErrorCode.CONSTRUCT_SUCCESS.getMessage());
-                              //  CONSTRUCT_SUCCESS.getMsg() 예외 메세지
+        //  CONSTRUCT_SUCCESS.getMsg() 예외 메세지
     }
 
 
-//          전체 조회하기
+    //          전체 조회하기
     @Transactional
     public List<PinResponseDto> getPins() {
         List<Pin> pins = pinRepository.findAllByOrderByCreatedAtDesc();
@@ -57,10 +57,10 @@ public class PinService {
         for (Pin p : pins) {
             p.getComments().sort(Comparator.comparing(Comment::getCreatedAt).reversed());
             List<CommentResponseDto> commentResponseDtos = new ArrayList<>();
-            for(Comment c : p.getComments()){
+            for (Comment c : p.getComments()) {
                 commentResponseDtos.add(new CommentResponseDto(c));
             }
-            pinResponseDtos.add(PinResponseDto.of(p,commentResponseDtos));
+            pinResponseDtos.add(PinResponseDto.of(p, commentResponseDtos));
         }
         return pinResponseDtos;
     }
